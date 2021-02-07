bba9c28ff7dc00e684143e289ac45677a785b1cc
Anggarahayudiputra <anggarahayudiputra@gmail.com>
2021-02-07 15:14:57 +0800

    deploy to netlify

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 import "../css/app.css";
+import "./menu-toggler.js";
+import "./modal.js";
+import "./anchor-link-smooth.js";
+import "./carousel.js";
+import "./accordion.js";
+import "./slider.js";
+import "./shopping-cart.js";
+import "./shipping-detail.js";
